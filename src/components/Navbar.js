77b6d3c6fed655f4cbcdd13151dcd0339cb6df87
Andrew Anderson <76078984+apanderson89@users.noreply.github.com>
2022-02-16 23:19:20 -0600

    nav bar part 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
@@ -7,12 +7,12 @@ function Navbar() {
        <nav className='navbar'>
            <div className='navbar-container'>
                <Link to='/' className="navbar-logo">
-                   TRVL
+                   TRVL <i className='fab fa-typo3' /> 
                </Link>
            </div>
        </nav>
        </>
-    )
+    );
 }
 
 export default Navbar
