a8d3dbf3d73a14865ef43fbd7522d2fb801fa475
Andrew Anderson <76078984+apanderson89@users.noreply.github.com>
2022-02-23 22:24:17 -0600

    navbar styling 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import "./Navbar.css";
+
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+           setButton(false); 
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
        <>
        <nav className='navbar'>
@@ -38,7 +57,7 @@ function Navbar() {
                        </Link>
                    </li>
                </ul>
-               
+               {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
            </div>
        </nav>
        </>
