f84d0c15b5f539ccfd7fb2c3eb6776aaf5ff51f3
Andrew Anderson <76078984+apanderson89@users.noreply.github.com>
2022-02-28 21:21:02 -0600

    cards not working

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function CardItem() {
   return (
     <>
     <li className="cards_item">
-        <link className="cards_item_link">
+        <Link className="cards_item_link">
             <figure className="cards_item_pic-wrap">
                <img src='/' alt='Travel Image' className="cards_item_img"/> 
             </figure>
-        </link>
+            <div className="cards_item_info">
+              <h5 className="cards_item_text" />
+            </div>
+        </Link>
     </li>
     </>
   )
 }
 
-export default CardItem
+export default CardItem;
